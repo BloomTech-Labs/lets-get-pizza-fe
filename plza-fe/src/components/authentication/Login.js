6eb4951c4aff67353747432b49827091c087e915
Spencer Summerville <me@grenuttag.com>
2019-11-07 00:17:45 -0500

    Added user login functionality
    - Created API utility function for Axios that appends a header to each
    request with the JWT stored using the Local Storage API
    - Server errors are now displayed
    - Submit button is disabled while submitting form or validation fails
    - Created dotenv file for Axios base URL. Create React App recommends
    that a base dotenv file is checked into source control

--- plza-fe/src/components/authentication/Login.js
+++ plza-fe/src/components/authentication/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import API from "../../utils/API";
 
 const loginSchema = Yup.object().shape({
   username: Yup.string().required("Username is required"),
@@ -17,20 +18,34 @@ export default function Login() {
       <Formik
         initialValues={{ username: "", password: "" }}
         validationSchema={loginSchema}
-        onSubmit={values => {
-          console.log(values);
-          alert("Form submitted!");
+        onSubmit={(values, { setSubmitting, setFieldError }) => {
+          setSubmitting(true);
+          const { username, password } = values;
+
+          API.post("/api/auth/user/login", { username, password })
+            .then(response => {
+              localStorage.setItem("token", response.data.token);
+              setSubmitting(false);
+            })
+            .catch(error => {
+              setFieldError("message", error.response.data.message);
+              setSubmitting(false);
+            });
         }}
       >
-        {() => (
+        {({ errors, isSubmitting, isValid }) => (
           <Form>
+            <div className="message">{errors.message}</div>
+
             <Field type="text" name="username" placeholder="Username" />
             <ErrorMessage name="username" />
 
             <Field type="password" name="password" placeholder="Password" />
             <ErrorMessage name="password" />
 
-            <button type="submit">Submit</button>
+            <button type="submit" disabled={isSubmitting || !isValid}>
+              Submit
+            </button>
           </Form>
         )}
       </Formik>
