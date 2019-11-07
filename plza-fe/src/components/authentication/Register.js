af3af835c9dee947bd07ed48a10aff633c362c4d
Spencer Summerville <me@grenuttag.com>
2019-11-06 19:46:01 -0500

    Added Yup validation library
    - Updated Create React App to 3.2.0
    - Created a simple validation schema for registration. Password verification
    is not working at this moment because it is comparing an undefined value.

--- plza-fe/src/components/authentication/Register.js
+++ plza-fe/src/components/authentication/Register.js
@@ -1,5 +1,21 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const registrationSchema = values =>
+  Yup.object().shape({
+    username: Yup.string().required("Required"),
+    password: Yup.string()
+      .min(4, "Password is too short")
+      .required("Password is required"),
+    password_verify: Yup.string()
+      .oneOf([values.password], "Passwords are not the same")
+      .min(4, "Too short")
+      .required("Password confirmation is required"),
+    email: Yup.string()
+      .email("Invalid email")
+      .required("Required")
+  });
 
 export default function Register() {
   return (
@@ -17,28 +33,41 @@ export default function Register() {
           city: "",
           state: ""
         }}
+        // validationSchema={registrationSchema}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert("Form submitted!");
+            console.log(values);
             setSubmitting(false);
           });
         }}
       >
-        {({ isSubmitting }) => (
+        {({ errors, touched, isSubmitting }) => (
           <Form>
             <Field type="username" name="username" placeholder="Username" />
+            <ErrorMessage name="username" />
+
             <Field type="email" name="email" placeholder="Email address" />
+            <ErrorMessage name="email" />
+
             <Field type="password" name="password" placeholder="Password" />
+            <ErrorMessage name="password" />
+
             <Field
               type="password"
               name="password_verify"
               placeholder="Password verification"
             />
-            <Field name="dietary_preference" as="select" multiple>
+            <ErrorMessage name="password_verify" />
+
+            <Field as="select" multiple name="dietary_preference">
               <option value="gluten-free">Gluten-free</option>
               <option value="vegetarian">Vegetarian</option>
               <option value="vegan">Vegan</option>
             </Field>
+
+            <Field type="text" name="city" placeholder="City" />
+            <Field type="text" name="state" placeholder="State" />
 
             <button type="submit" disabled={isSubmitting}>
               Submit
