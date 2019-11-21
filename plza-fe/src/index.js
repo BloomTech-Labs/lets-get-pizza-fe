3bb6cb80bd2540ee164374a640498b6c83636eac
Robin U Familara <robin.familara@cgius.com>
2019-11-20 21:14:32 -0600

    import semantic-ui-css and semantic.min.css

--- plza-fe/src/index.js
+++ plza-fe/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css';
 
 import App from "./App";
-import "./index.css";
 
 ReactDOM.render(
   <Router>
