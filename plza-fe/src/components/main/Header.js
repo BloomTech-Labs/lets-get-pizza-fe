bc2bba3f2dda97199aa8c7c592b4be9bff130790
Spencer Summerville <me@grenuttag.com>
2019-12-01 22:42:33 -0500

    Masthead component is no longer centered

--- plza-fe/src/components/main/Header.js
+++ plza-fe/src/components/main/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Container, Menu, Dropdown } from "semantic-ui-react";
+import { Menu, Dropdown } from "semantic-ui-react";
 import { hasToken, logoutUser } from "../../utils/auth";
 
 const UserProfile = () => (
@@ -26,39 +26,37 @@ const AuthenticateOptions = () => (
 export default function Masthead() {
   return (
     <Menu stackable style={{ borderRadius: 0 }}>
-      <Container>
-        <Menu.Item>
-          <div style={{ width: "102px", padding: "0 12px" }}>
-            <img width="100%" src="https://i.imgur.com/os2t6S3.png" />
-          </div>
-        </Menu.Item>
-        <Menu.Item as={NavLink} exact to="/">
-          Home
-        </Menu.Item>
-        <Menu.Item as={NavLink} to="/locations/map">
-          Map
-        </Menu.Item>
-        <Menu.Item as={NavLink} to="/locations/search">
-          Search
-        </Menu.Item>
-        <Dropdown text="About" pointing className="link item">
-          <Dropdown.Menu>
-            <Dropdown.Item as={NavLink} to="/pages/eaters">
-              User Features
-            </Dropdown.Item>
-            <Dropdown.Item as={NavLink} to="/pages/businesses">
-              Business Features
-            </Dropdown.Item>
-            <Dropdown.Item as={NavLink} to="/pages/about">
-              About Our Team
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+      <Menu.Item>
+        <div style={{ width: "102px", padding: "0 12px" }}>
+          <img width="100%" src="https://i.imgur.com/os2t6S3.png" />
+        </div>
+      </Menu.Item>
+      <Menu.Item as={NavLink} exact to="/">
+        Home
+      </Menu.Item>
+      <Menu.Item as={NavLink} to="/locations/map">
+        Map
+      </Menu.Item>
+      <Menu.Item as={NavLink} to="/locations/search">
+        Search
+      </Menu.Item>
+      <Dropdown text="About" pointing className="link item">
+        <Dropdown.Menu>
+          <Dropdown.Item as={NavLink} to="/pages/eaters">
+            User Features
+          </Dropdown.Item>
+          <Dropdown.Item as={NavLink} to="/pages/businesses">
+            Business Features
+          </Dropdown.Item>
+          <Dropdown.Item as={NavLink} to="/pages/about">
+            About Our Team
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
 
-        <Menu.Menu position="right">
-          {hasToken ? <UserProfile /> : <AuthenticateOptions />}
-        </Menu.Menu>
-      </Container>
+      <Menu.Menu position="right">
+        {hasToken ? <UserProfile /> : <AuthenticateOptions />}
+      </Menu.Menu>
     </Menu>
   );
 }
