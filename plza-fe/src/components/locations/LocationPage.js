52d1ebd119dacd97ea7d1c72aa535d83be4baa9b
Spencer Summerville <me@grenuttag.com>
2019-12-10 22:17:05 -0500

    Work in progress styling of LocationDetail
    Trying to implement a two column layout for LocationDetail

--- plza-fe/src/components/locations/LocationPage.js
+++ plza-fe/src/components/locations/LocationPage.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { Card, Container, Header } from "semantic-ui-react";
+import {
+  Container,
+  Header,
+  Image,
+  Grid,
+  Segment,
+  Card
+} from "semantic-ui-react";
 
 import API from "../../utils/API";
+
+import "./LocationPage.css";
 
 // Location detail page
 // Displays all information about a given location through the
@@ -19,37 +28,49 @@ export default function LocationPage() {
   }, [id]);
 
   return (
-    <div className="location">
-      <Container>
-        <Header as="h1">{location.business_name}</Header>
-        <Card.Description>{location.address}</Card.Description>
-        <p>
-          <a
-            href={location.website_url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Website
-          </a>
-        </p>
-        <Card.Description as="h4">
-          About {location.business_name}
-        </Card.Description>
-        <p>{location.official_description || "-"}</p>
-        <Card.Description as="h4">Our Store</Card.Description>
-        <p>{location.store_bio || "-"}</p>
-        <Card.Description as="h4">Special Diet Options</Card.Description>
-        <p>{location.dietary_offerings || "-"}</p>
-        <Link to="/locations/map">Back to Map</Link> |{" "}
-        <Link to="/locations/search">Back to Search</Link> <br />
-        {!location.email && location.foursquare_id ? (
-          <Link to={`/locations/claim/${location.id}`}>
-            Claim this Location
-          </Link>
-        ) : (
-          ""
+    <Container style={{ margin: "20px 0" }}>
+      <Header size="large">
+        {location.thumbnail_url && (
+          <Image circular src={location.thumbnail_url} />
         )}
-      </Container>
-    </div>
+        {location.business_name}
+      </Header>
+      <Grid columns="2">
+        <Grid.Row>
+          <h1>Location Information</h1>
+          <Segment.Group>
+            <Segment>
+              <h4>About {location.business_name}</h4>
+              <p>{location.official_description || "-"}</p>
+            </Segment>
+            <Segment>
+              <h4>Our Store</h4>
+              <p>{location.store_bio || "-"}</p>
+            </Segment>
+            <Segment>
+              <h4>Special Diet Options</h4>
+              <p>{location.dietary_offerings || "-"}</p>
+            </Segment>
+          </Segment.Group>
+        </Grid.Row>
+        <Grid.Row>
+          <h1>Events, Reviews, and More</h1>
+        </Grid.Row>
+      </Grid>
+      <p>
+        <a
+          href={location.website_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Website
+        </a>
+      </p>
+      <Link to="/locations/map">Back to Map</Link> |{" "}
+      <Link to="/locations/search">Back to Search</Link> <br />
+      {!location.email && (
+        <Link to={`/locations/claim/${location.id}`}>Claim this Location</Link>
+      )}
+    </Container>
   );
 }
