70bd3ee9318f0c8cf864d7927d682d3e66b35229
curtdragon <tealturboser@gmail.com>
2019-12-11 22:36:00 -0600

    Made changes to static pages to add an about us page

--- plza-fe/src/components/staticPages/AboutUs.js
+++ plza-fe/src/components/staticPages/AboutUs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Image, Icon, Header } from "semantic-ui-react";
+import { Grid, Image, Button, Header } from "semantic-ui-react";
 
 //Importing profile images
 import rf from "../../media/rf.jpg";
@@ -9,56 +9,62 @@ import cs from "../../media/cs.jpg";
 import ss from "../../media/profile.png";
 
 //Constants for URLs
-// //Robin
-// const rfgh = "https://github.com/rfamilara";
-// const rfli = "https://www.linkedin.com/in/rf2017/";
-// const rftw = "https://twitter.com/Niborhoods";
+//Robin
+const rfgh = "https://github.com/rfamilara";
+const rfli = "https://www.linkedin.com/in/rf2017/";
+const rftw = "https://twitter.com/Niborhoods";
 
-// //Jordan
-// const jhgh = "https://github.com/thejhubbs";
+//Jordan
+const jhgh = "https://github.com/thejhubbs";
 
-// //Justin
-// const jlgh = "https://github.com/JCBLandry";
-// const jltw = "https://twitter.com/MelonRats";
+//Justin
+const jlgh = "https://github.com/JCBLandry";
+const jltw = "https://twitter.com/MelonRats";
 
-// //Curtis
-// const csgh = "https://github.com/curtdragon";
-// const cstw = "https://twitter.com/curtdragon";
-// const csli = "https://www.linkedin.com/in/curtis-scafe-web-developer/";
-// const csws = "https://curtiscancode.dev/";
+//Curtis
+const csgh = "https://github.com/curtdragon";
+const cstw = "https://twitter.com/curtdragon";
+const csli = "https://www.linkedin.com/in/curtis-scafe-web-developer/";
+const csws = "https://curtiscancode.dev/";
 
-// //Spencer 
-// const ssgh = "https://github.com/grenuttag";
-// const ssws = "https://spencer.summerville.dev";
+//Spencer 
+const ssgh = "https://github.com/grenuttag";
+const ssws = "https://spencer.summerville.dev";
 
 export default function AboutUs() {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Meet the Team</h1>
+      <Header as="h1">Meet the Team</Header>
       <Grid>
         <Grid.Row columns={3}>
           <Grid.Column>
             <Header as="h2">Robin Familara</Header>
             <Image src={rf} size="small" bordered centered circular />
-            <Icon
+            <Button
+              basic
+              href={rfgh}
+              target="_blank"
               circular
-              link
-              size="big"
-              name="github"
+              size="large"
+              icon="github"
             />
-            <Icon
+            <Button
+              basic
+              href={rftw}
+              target="_blank"
               circular
-              link
-              size="big"
-              name="twitter"
+              size="large"
+              icon="twitter"
             />
-            <Icon 
-              circular 
-              link 
-              size="big" 
-              name="linkedin" 
+            <Button
+              basic
+              href={rfli}
+              target="_blank"
+              circular
+              size="large"
+              icon="linkedin"
             />
-            <Header as="h3">
+            <Header as="h4">
               I’m a loyal Pelicans/Saints fan. My hobby is watching epic battles
               among Marvel superheroes and villains on YouTube while eating
               pizza.
@@ -71,12 +77,15 @@ export default function AboutUs() {
           <Grid.Column>
             <Header as="h2">Jordan Hubbard</Header>
             <Image src={jh} size="small" bordered centered circular />
-            <Icon
+            <Button
+              basic
+              href={jhgh}
+              target="_blank"
               circular
-              size="big"
-              name="github"
+              size="large"
+              icon="github"
             />
-            <Header as="h3">
+            <Header as="h4">
               I do art, tech, music, and tv. I like figuring out how things
               work, and then building and creating things with em.
             </Header>
@@ -85,18 +94,23 @@ export default function AboutUs() {
           <Grid.Column>
             <Header as="h2">Justin Landry</Header>
             <Image src={jl} size="small" bordered centered circular />
-            <Icon
+            <Button
+              basic
+              href={jlgh}
+              target="_blank"
               circular
-              size="big"
-              name="github"
+              size="large"
+              icon="github"
             />
-            <Icon
+            <Button
+              basic
+              href={jltw}
+              target="_blank"
               circular
-              link
-              size="big"
-              name="twitter"
+              size="large"
+              icon="twitter"
             />
-            <Header as="h3">Fighting games, LARP, Proper pizza</Header>
+            <Header as="h4">Fighting games, LARP, Proper pizza</Header>
           </Grid.Column>
         </Grid.Row>
 
@@ -104,29 +118,39 @@ export default function AboutUs() {
           <Grid.Column>
             <Header as="h2">Curtis Scafe</Header>
             <Image src={cs} size="small" bordered centered circular />
-            <Icon
+            <Button
+              basic
+              href={csgh}
+              target="_blank"
               circular
-              size="big"
-              name="github"
+              size="large"
+              icon="github"
             />
-            <Icon
+            <Button
+              basic
+              href={cstw}
+              target="_blank"
               circular
-              link
-              size="big"
-              name="twitter"
+              size="large"
+              icon="twitter"
             />
-            <Icon 
-              circular 
-              link 
-              size="big" 
-              name="linkedin" 
+            <Button
+              basic
+              href={csli}
+              target="_blank"
+              circular
+              size="large"
+              icon="linkedin"
             />
-            <Icon 
-              circular 
-              size="big" 
-              name="globe" 
+            <Button
+              basic
+              href={csws}
+              target="_blank"
+              circular
+              size="large"
+              icon="globe"
             />
-            <Header as="h3">
+            <Header as="h4">
               Born in Jamaica, grew up in NJ, currently in Nashville.
               <br></br>
               Gearhead\car guy. Love to hike. Foodie and runner.
@@ -136,17 +160,23 @@ export default function AboutUs() {
           <Grid.Column>
             <Header as="h2">Spencer Summerville</Header>
             <Image src={ss} size="small" bordered centered circular />
-            <Icon
+            <Button
+              basic
+              href={ssgh}
+              target="_blank"
               circular
-              size="big"
-              name="github"
+              size="large"
+              icon="github"
             />
-            <Icon 
-              circular 
-              size="big" 
-              name="globe" 
+            <Button
+              basic
+              href={ssws}
+              target="_blank"
+              circular
+              size="large"
+              icon="globe"
             />
-            <Header as="h3">Pizza and programming aficionado.</Header>
+            <Header as="h4">Pizza and programming aficionado.</Header>
           </Grid.Column>
         </Grid.Row>
       </Grid>
