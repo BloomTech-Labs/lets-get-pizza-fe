2b4b085d75dd20ed2f90bce2d49af0e872c434b0
Spencer Summerville <me@grenuttag.com>
2020-01-08 00:13:34 -0500

    Map improvements
    - Rearranged state declarations
    - Added maximum and minimum zoom values
    - When changing to another location, the map uses a fly to animation
    - Markers are now memoized to try and help with performance
    - Popup window pointer size increased
    - A slight linear transition has been added to every viewport update to make map dragging a little bit more smooth

--- plza-fe/src/components/map/FancyMap.js
+++ plza-fe/src/components/map/FancyMap.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Icon, Loader, Form } from "semantic-ui-react";
-import ReactMapGL, { FlyToInterpolator, Marker, Popup } from "react-map-gl";
+import ReactMapGL, {
+  LinearInterpolator,
+  FlyToInterpolator,
+  Marker,
+  Popup
+} from "react-map-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -9,20 +14,9 @@ import LocationCard from "../locations/search/LocationCard";
 
 export default function FancyMap(props) {
   const [isLoading, setIsLoading] = useState(true);
+
+  // Location state
   const [locations, setLocations] = useState([]);
-  const [userLocation, setUserLocation] = useState({});
-
-  const [viewport, setViewport] = useState({
-    latitude: 0,
-    longitude: 0,
-    width: props.width,
-    height: props.height,
-    zoom: 13
-  });
-
-  const [selectedMarker, setSelectedMarker] = useState({});
-  const [isPopupVisible, setPopupVisiblility] = useState(false);
-
   const fetchLocations = searchQuery =>
     API.get("/locations/map", { params: { search: searchQuery } })
       .then(response => {
@@ -32,13 +26,12 @@ export default function FancyMap(props) {
       })
       .catch(error => console.log("Error:", error));
 
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const updateSearchQuery = event => setSearchQuery(event.target.value);
-  const searchLocations = event => fetchLocations(searchQuery);
+  // User location state
+  const [userLocation, setUserLocation] = useState({});
 
   // Whenever user location changes, update viewport
-  // to those coordinates
+  // to those coordinates and use a fly to transition
+  // so it looks pretty
   useEffect(() => {
     setViewport(viewport => ({
       ...viewport,
@@ -49,24 +42,40 @@ export default function FancyMap(props) {
     }));
   }, [userLocation]);
 
+  // Initial viewport state
+  const [viewport, setViewport] = useState({
+    latitude: 0,
+    longitude: 0,
+    width: props.width,
+    height: props.height,
+    pitch: 0,
+    zoom: 12,
+    minZoom: 12,
+    maxZoom: 16,
+    transitionDuration: 500,
+    transitionInterpolator: new LinearInterpolator()
+  });
+
   const onViewportChange = viewport => setViewport({ ...viewport });
 
+  // Marker popup state
+  const [selectedMarker, setSelectedMarker] = useState({});
+  const [isPopupVisible, setPopupVisiblility] = useState(false);
+
+  // Geocoder state
+  const [searchQuery, setSearchQuery] = useState("");
+  const updateSearchQuery = event => setSearchQuery(event.target.value);
+  const searchLocations = event => fetchLocations(searchQuery);
+
+  // On first render, fetch locations without a search query.
+  // This will default to the location retrieved by the backend.
   useEffect(() => {
     fetchLocations();
   }, []);
 
-  if (isLoading) {
-    return <Loader active>Loading map...</Loader>;
-  }
-
-  return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={onViewportChange}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      mapStyle="mapbox://styles/grenuttag/ck4do0nf04awl1co2h6kb7b6y"
-    >
-      {locations.map(location => (
+  const Markers = React.memo(
+    ({ locations, setPopupVisiblility, setSelectedMarker }) =>
+      locations.map(location => (
         <Marker
           key={location.location_id || location.foursquare_id}
           latitude={location.latitude}
@@ -82,15 +91,32 @@ export default function FancyMap(props) {
             }}
           />
         </Marker>
-      ))}
+      ))
+  );
+
+  if (isLoading) {
+    return <Loader active>Loading map...</Loader>;
+  }
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={onViewportChange}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/grenuttag/ck4do0nf04awl1co2h6kb7b6y"
+    >
+      <Markers
+        locations={locations}
+        setPopupVisiblility={setPopupVisiblility}
+        setSelectedMarker={setSelectedMarker}
+      />
 
       {isPopupVisible && (
         <Popup
-          tipSize={5}
+          tipSize={10}
           anchor="top"
           offsetTop={45}
           offsetLeft={15}
-          dynamicPosition={false}
           latitude={selectedMarker.latitude}
           longitude={selectedMarker.longitude}
           onClose={() => setPopupVisiblility(false)}
