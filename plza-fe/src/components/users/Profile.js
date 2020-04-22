f342445be4c68247a2ddbe424d9ffdbdc906c794
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-04-21 21:20:48 -0700

    Add onClick event to make menu. Need to create components to import/render based on menu item clicked. Will replace items object's content value

--- plza-fe/src/components/users/Profile.js
+++ plza-fe/src/components/users/Profile.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { curr_user, curr_location } from '../../utils/auth'
-import { Grid, Menu } from "semantic-ui-react";
+import { Grid, Menu, Header } from "semantic-ui-react";
+import Login from './Login';
+import Register from './Register'
 
 //THIS NEEDS TO SPLIT INTO TWO DIFFERENT COMPONENTS
 export default function Profile() {
-  return (
+  const [component, setComponent] = useState('Home');
 
+  const selectComponet = (e, data) => {
+    setComponent(data.content);
+  }
+  console.log(component)
+  return (
     // profile div with 2 child components to render side-by-side
     // left-menu-bar div to have tabs/actions to conditionally render list components to the right
     // list component container
-    <Grid container columns={2} relaxed stackable>
+    <Grid container columns={2} width={16} stackable>
       <Grid.Row>
-        <Grid.Column width={5} style={{ paddingTop: '2vh', height: '45vh' }}>
+        <Grid.Column floated='left' width={5} style={{ paddingTop: '2vh', height: '45vh' }}>
+
           <Menu items={[
             { key: '1', name: 'Home', content: 'Home' },
             { key: '2', name: 'My Friends', content: 'My Friends' },
@@ -20,12 +28,24 @@ export default function Profile() {
             { key: '5', name: 'Fav Pizza Shop', content: 'Fav Pizza Shop' },
             { key: '6', name: 'Personal Reviews', content: 'Personal Reviews' },
             { key: '7', name: 'Profile', content: 'Profile' }
-          ]} size='large' fluid='true' pointing tabular vertical />
+          ]}
+            size='large'
+            fluid='true'
+            pointing
+            tabular
+            vertical
+            onItemClick={selectComponet}
+            defaultActiveIndex={0}
+          />
 
         </Grid.Column>
         <Grid.Column width={11} align={'center'} style={{ paddingTop: '2vh' }}>
+          <Header as='h2' textAlign='center'>
+            <Header.Content>{component}</Header.Content>
+          </Header>
+
           List Component
-      </Grid.Column>
+        </Grid.Column>
       </Grid.Row>
     </Grid>
   )
