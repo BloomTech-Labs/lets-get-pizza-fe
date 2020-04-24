317b0193a5e7cc9b83c866d0ece0662648175f1e
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-04-23 21:07:12 -0700

    Update User Dashboard Menu (#38)
    * Add onClick event to make menu. Need to create components to import/render based on menu item clicked. Will replace items object's content value
    
    * Remove commented out code
    
    * Add dashboard icons to media directory
    
    Create icons.js to import and assign all images to an object for export
    
    Refactor Menu.Item in Profile.js to allow for icon images to be utilized
    
    * Add new profile-icon that is resized and centered
    
    Remove unused imports
    
    * Remove console log
    
    * Refactor code to map through array of menuItem info instead of writing out each Menu.Item component
    
    * Move menu contents into a seperate component, DashMenu.js
    
    * Pass state and selectComponent function down to DashMenu.js
    
    Update profile-icon.png to line up with the other icons correctly
    
    * Update icons to be sized, transparent, and positioned correctly in the Dashboard menu

---
+++ plza-fe/src/components/users/Dashboard.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import DashMenu from './dashboard-utils/DashMenu';
+import UserSectionsController from './sections/UserSectionsController'
+
+
+export default function Dashboard() {
+  const [active, setActive] = useState('Home');
+
+  const selectComponent = (e, data) => {
+    console.log(data.name)
+    setActive(data.name);
+  }
+
+  return (
+    // profile div with 2 child components to render side-by-side
+    // left-menu-bar div to have tabs/actions to conditionally render list components to the right
+    // list component container
+    <Grid container columns={2} width={16} stackable>
+      <Grid.Row>
+        <Grid.Column floated='left' width={5} style={{ paddingTop: '2vh', height: '45vh' }} >
+          <DashMenu selectComponent={selectComponent} active={active} />
+        </Grid.Column>
+        <Grid.Column width={11} align={'left'} style={{ paddingTop: '2vh' }}>
+          <UserSectionsController />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
+}
