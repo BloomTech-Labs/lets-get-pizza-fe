c460f50efdef23e8d65da386042c77d2b10f605a
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-04-28 19:09:41 -0700

    Refactor List UI components to Grid to space out buttons better
    Still need to implement API call & render dietary preferences as a list w/ edit as dropdown

--- plza-fe/src/components/users/settings/UserSettingsList.js
+++ plza-fe/src/components/users/settings/UserSettingsList.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import { List, Button, Icon, Input, Item } from 'semantic-ui-react';
+import { List, Button, Icon, Input, Item, Grid } from 'semantic-ui-react';
+import UserEditButton from './UserEditButton';
 
 
 export default function UserSettingsList(props) {
 
+
+
     return (
-        <List.Item>
-            <List.Icon name={props.item.icon} size='large' verticalAlign='middle' />
-            <List.Content>
-                {props.current === props.item.name ? (
-                    <List.Content>
-                        <Input onChange={props.handleChange} name={props.item.name} value={props.item.value}></Input>
-                        <Button animated='fade' size={'tiny'} onClick={props.handleClick}>
-                            <Button.Content hidden>Save</Button.Content>
-                            <Button.Content visible>
-                                <Icon name='save' />
-                            </Button.Content>
-                        </Button>
-                    </List.Content>
-                ) : (
-                        <div style={{ display: 'flex' }}>
-                            <div>
-                                <List.Header >{props.item.name}</List.Header>
-                                <List.Description>{props.user.email}</List.Description>
+        <Grid columns={2} >
+
+
+            {props.current === props.item.name ? (
+                <Grid.Row style={{ borderBottom: '1px solid lightgrey' }}>
+                    <Grid.Column style={{ display: 'flex', alignItems: 'center' }}>
+                        <Icon name={props.item.icon} size='large' />
+                        <Input onChange={props.handleChange} name={props.item.name} value={props.item.value} />
+                    </Grid.Column>
+                    <UserEditButton text={'Save'} item={props.item} handleClick={props.handleSubmit} />
+                </Grid.Row>
+            ) : (
+                    <Grid.Row style={{ borderBottom: '1px solid lightgrey' }}>
+                        <Grid.Column style={{ display: 'flex', alignItems: 'center' }}>
+                            <Icon name={props.item.icon} size='large' />
+                            <div style={{ marginLeft: "1rem" }}>
+                                <List.Header >{props.item.title}</List.Header>
+                                <List.Description>{props.item.value}</List.Description>
                             </div>
-                            <Button style={{ marginLeft: "1rem" }} animated='fade' size={'tiny'} onClick={props.handleClick}>
-                                <Button.Content hidden>Edit</Button.Content>
-                                <Button.Content visible icon="edit">
-                                    <Icon id={props.item.name} name='edit' />
-                                </Button.Content>
-                            </Button>
-                        </div>
-                    )}
-            </List.Content>
-        </List.Item>
+                        </Grid.Column>
+                        <UserEditButton text={'Edit'} item={props.item} handleClick={props.handleClick} />
+                    </Grid.Row>
+                )}
+        </Grid>
     )
 }
--- plza-fe/src/components/users/settings/userSettings.js
+++ plza-fe/src/components/users/settings/userSettings.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import { List, Image, Button, Icon, Input } from 'semantic-ui-react';
 import { curr_user } from "../../../utils/auth";
 import UserSettingsList from './UserSettingsList';
+import UserEditButton from './UserEditButton';
 import API from "../../../utils/API";
 
 
 
 export default function UserSettings() {
     const [current, setCurrent] = useState('')
-    const [user, setUser] = useState({ ...curr_user });
-    const listItems = [
-        { name: 'Email', icon: 'mail', value: user.email },
-        { name: 'Location', icon: 'location arrow', value: user.display_location },
-        { name: 'Favorite Toppings', icon: 'mail', value: user.favorite_pizza_toppings },
-        { name: 'Dietary Preferences', icon: 'ban', value: user.dietary_preference },
-    ]
+    const [user, setUser] = useState({ ...curr_user, dietary_preference: ['corn', 'pork', 'dairy'] });
+    console.log(user)
+
+    const handleSubmit = e => {
+        setCurrent('')
+    }
 
     const handleClick = e => {
         console.log(e.target)
@@ -23,51 +23,57 @@ export default function UserSettings() {
     }
 
     const handleChange = e => {
-        console.log(e.target.name)
+        console.log(e.target)
         setUser({ ...user, [e.target.name]: e.target.value })
     }
 
-    const handleSubmit = e => {
-        setCurrent('')
-    }
-
+    const listItems = [
+        { title: 'Email', name: 'email', icon: 'mail', value: user.email },
+        { title: 'Location', name: 'display_location', icon: 'location arrow', value: user.display_location },
+        { title: 'Favorite Toppings', name: 'favorite_pizza_toppings', icon: 'mail', value: user.favorite_pizza_toppings },
+        { title: 'Dietary Preferences', name: 'dietary_preference', icon: 'ban', value: user.dietary_preference },
+    ]
 
     return (
         <List divided relaxed='very'>
-
-
             {/* Top section with user avatar image & display name */}
             <List.Item style={{ display: "flex" }}>
                 <List.Content>
-                    <Image avatar size={'tiny'} src="http://place-puppy.com/1000x1000" />
+                    {/* <Image avatar size={'tiny'} src={user.profile_image} /> */}
+                    <Image avatar size={'tiny'} src={"http://place-puppy.com/1000x1000"} />
                 </List.Content>
                 <List.Content style={{ marginLeft: "1rem", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
                     <List.Content>
                         <List.Header>Display Name</List.Header>
                         <List.Description>{user.display_name}</List.Description>
                     </List.Content>
-                    {current === 'display_name' ? (
-                        <List.Content>
-                            <Input onChange={handleChange} name='display_name' value={user.display_name}></Input>
-                            <Button animated='fade' size={'tiny'} onClick={handleClick}>
-                                <Button.Content hidden>Save</Button.Content>
-                                <Button.Content visible>
-                                    <Icon name='save' />
-                                </Button.Content>
-                            </Button>
+                    {current === 'Display Name' ? (
+                        <List.Content style={{ display: 'flex' }}>
+                            <Input onChange={handleChange}
+                                name='display_name'
+                                value={user.display_name}
+                            />
+                            <UserEditButton text={'Save'}
+                                item={{ title: 'Display Name', icon: 'edit', value: user.display_name }}
+                                handleClick={handleSubmit}
+                            />
                         </List.Content>
                     ) : (
-                            <Button animated='fade' size={'tiny'} onClick={handleClick}>
-                                <Button.Content hidden>Edit</Button.Content>
-                                <Button.Content visible icon="edit">
-                                    <Icon id={'display_name'} name='edit' />
-                                </Button.Content>
-                            </Button>
+                            <UserEditButton text={'Edit'}
+                                item={{ title: 'Display Name', icon: 'edit', value: user.display_name }}
+                                handleClick={handleClick}
+                            />
                         )}
                 </List.Content>
             </List.Item>
 
-            {listItems.map(item => <UserSettingsList current={current} user={user} handleChange={handleChange} item={item} handleClick={handleClick} />
+            {listItems.map(item => <UserSettingsList current={current}
+                user={user}
+                handleChange={handleChange}
+                item={item}
+                handleClick={handleClick}
+                handleSubmit={handleSubmit}
+            />
             )}
         </List>
     )
