9c280881b8a2c66f55d59452278a2cbbcb01e24a
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-04-29 19:02:02 -0700

    Create UserDisplayInfo to render top content of the UserSettings

---
+++ plza-fe/src/components/users/settings/UserDisplayInfo.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { List, Image, Input } from 'semantic-ui-react';
+import UserEditButton from './UserEditButton';
+
+export default function UserDisplayInfo(props) {
+
+    return (
+        <List.Item style={{ display: "flex" }}>
+            <List.Content>
+                <Image avatar size={'tiny'} src={`${props.user.profile_image}`} />
+            </List.Content>
+            <List.Content style={{ marginLeft: "1rem", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+                <List.Content>
+                    <List.Header>Display Name</List.Header>
+                    <List.Description>{props.user.display_name}</List.Description>
+                </List.Content>
+                {props.current === 'Display Name' ? (
+                    <List.Content style={{ display: 'flex' }}>
+                        <Input onChange={props.handleChange}
+                            name='display_name'
+                            value={props.user.display_name}
+                        />
+                        <UserEditButton text={'Save'}
+                            item={{ name: 'Display Name', value: props.user.display_name }}
+                            handleClick={props.handleSubmit}
+                        />
+                    </List.Content>
+                ) : (
+                        <UserEditButton text={'Edit'}
+                            item={{ name: 'Display Name', value: props.user.display_name }}
+                            handleClick={props.handleClick}
+                        />
+                    )}
+            </List.Content>
+        </List.Item>
+    )
+}
--- plza-fe/src/components/users/settings/userSettings.js
+++ plza-fe/src/components/users/settings/userSettings.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { List, Image, Input, Button } from 'semantic-ui-react';
+import { List, Button } from 'semantic-ui-react';
 import { curr_user } from "../../../utils/auth";
 import UserSettingsList from './UserSettingsList';
-import UserEditButton from './UserEditButton';
+import UserDisplayInfo from './UserDisplayInfo';
 import API from "../../../utils/API";
 
 
@@ -53,35 +53,9 @@ export default function UserSettings() {
         <>
             <List divided relaxed='very'>
                 {/* Top section with user avatar image & display name */}
-                <List.Item style={{ display: "flex" }}>
-                    <List.Content>
-                        <Image avatar size={'tiny'} src={`${user.profile_image}`} />
-                    </List.Content>
-                    <List.Content style={{ marginLeft: "1rem", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-                        <List.Content>
-                            <List.Header>Display Name</List.Header>
-                            <List.Description>{user.display_name}</List.Description>
-                        </List.Content>
-                        {current === 'Display Name' ? (
-                            <List.Content style={{ display: 'flex' }}>
-                                <Input onChange={handleChange}
-                                    name='display_name'
-                                    value={user.display_name}
-                                />
-                                <UserEditButton text={'Save'}
-                                    item={{ name: 'Display Name', value: user.display_name }}
-                                    handleClick={handleSubmit}
-                                />
-                            </List.Content>
-                        ) : (
-                                <UserEditButton text={'Edit'}
-                                    item={{ name: 'Display Name', value: user.display_name }}
-                                    handleClick={handleClick}
-                                />
-                            )}
-                    </List.Content>
-                </List.Item>
+                <UserDisplayInfo user={user} handleChange={handleChange} handleClick={handleClick} handleSubmit={handleSubmit} current={current} />
 
+                {/* Map through user settings array & render rows */}
                 {listItems.map(item => <UserSettingsList current={current}
                     key={item.name}
                     user={user}
