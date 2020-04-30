32472a50f45f7ce9b19f881a7d02e13db8a39e56
Freddie <fthompson803@live.com>
2020-04-29 21:19:20 -0400

    staging for merge

--- plza-fe/src/components/users/Dashboard.js
+++ plza-fe/src/components/users/Dashboard.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { Grid } from "semantic-ui-react";
-import DashMenu from './dashboard-utils/DashMenu';
-import UserSectionsController from './sections/UserSectionsController'
-
+import DashMenu from "./dashboard-utils/DashMenu";
+import UserSectionsController from "./sections/UserSectionsController";
+import FriendsController from "./FriendsComp/FriendsController";
+import FriendsListController from "./FriendsComp/FriendsController";
 
 export default function Dashboard() {
-  const [active, setActive] = useState('Home');
+  const [active, setActive] = useState("Home");
 
   const selectComponent = (e, data) => {
-    console.log(data.name)
+    console.log(data.name);
     setActive(data.name);
-  }
+  };
 
   return (
     // profile div with 2 child components to render side-by-side
@@ -18,13 +19,18 @@ export default function Dashboard() {
     // list component container
     <Grid container columns={2} width={16} stackable>
       <Grid.Row>
-        <Grid.Column floated='left' width={5} style={{ paddingTop: '2vh', height: '45vh' }} >
+        <Grid.Column
+          floated="left"
+          width={5}
+          style={{ paddingTop: "2vh", height: "45vh" }}
+        >
           <DashMenu selectComponent={selectComponent} active={active} />
         </Grid.Column>
-        <Grid.Column width={11} align={'left'} style={{ paddingTop: '2vh' }}>
+        <Grid.Column width={11} align={"left"} style={{ paddingTop: "2vh" }}>
           <UserSectionsController />
+          <FriendsListController />
         </Grid.Column>
       </Grid.Row>
     </Grid>
-  )
+  );
 }
--- plza-fe/src/components/users/FriendsComp/FriendsList.js
+++ plza-fe/src/components/users/FriendsComp/FriendsList.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { curr_user } from "../../../utils/auth";
 import { List } from "semantic-ui-react";
 import FriendOnList from "./FriendOnList";
+import API from "../../../utils/API";
 
 const dummyList = [
   {
@@ -19,13 +20,75 @@ const dummyList = [
     image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
     bio: "Pineapple was made for pizza",
   },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
+  {
+    name: "Buddy1",
+    image: "https://ca.slack-edge.com/T4JUEB3ME-UHET379TP-a6cf2301dafe-512",
+    bio: "Pineapple was made for pizza",
+  },
 ];
+
 export default function FriendsList() {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    API.get(`/friends/${curr_user.id}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
-    <List floated="left" size="big">
-      {dummyList.map((friend) => {
-        return <FriendOnList key={friend.name} friends={friend} />;
-      })}
-    </List>
+    <>
+      <List floated="left" size="big">
+        {console.log("here")}
+
+        {dummyList.map((friend) => {
+          return <FriendOnList key={friend.name} friends={friend} />;
+        })}
+      </List>
+    </>
   );
 }
