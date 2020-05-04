f2cbebb750519faee9dc360521e64d5af01c178a
Buddy Agyin <agyin3@gmail.com>
2020-05-04 02:26:36 -0600

    refactor login/registration forms

--- plza-fe/src/components/users/Register.js
+++ plza-fe/src/components/users/Register.js
@@ -4,6 +4,7 @@ import { string } from "yup";
 
 import SimpleContainer from "../main/SimpleContainer";
 import AuthenticateForm from "../authentication/AuthenticateForm";
+import AuthForm from "../authentication/AuthForm";
 
 const Register = () => (
   <SimpleContainer
@@ -11,63 +12,9 @@ const Register = () => (
     title="Register account"
     description="Create an account for great features and fun!"
   >
-    <AuthenticateForm
+    <AuthForm
       isRegistrationForm
-      endpoint="/auth/user/register"
-      extraValues={{
-        profile_image: "",
-        display_name: "",
-        dietary_preference: [],
-        favorite_pizza_toppings: "",
-        display_location: ""
-      }}
-      extraSchema={{
-        display_name: string(),
-        favorite_pizza_toppings: string(),
-        display_location: string()
-      }}
-    >
-      <Input
-        name="display_name"
-        label="Display name"
-        inputProps={{
-          placeholder:
-            "Either a nickname or your real name, or nothing at all, no pressure."
-        }}
-      />
-
-      <Dropdown
-        name="dietary_preference"
-        label="Dietary preferences"
-        inputProps={{
-          multiple: true,
-          placeholder: "Select dietary preferences"
-        }}
-        options={[
-          { text: "Gluten-free", value: "gluten-free" },
-          { text: "Vegetarian", value: "vegetarian" },
-          { text: "Vegan", value: "vegan" }
-        ]}
-      />
-
-      <Input
-        name="favorite_pizza_toppings"
-        label="Favorite pizza toppings"
-        inputProps={{
-          icon: "heart",
-          placeholder: "Pepperoni, olives, pineapple, anchovies"
-        }}
-      />
-
-      <Input
-        name="display_location"
-        label="Location"
-        inputProps={{
-          icon: "globe",
-          placeholder: "A rough approximation of where you live"
-        }}
-      />
-    </AuthenticateForm>
+    />
   </SimpleContainer>
 );
 
