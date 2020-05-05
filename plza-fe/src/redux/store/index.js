964b05674844ab3bf0220bd5a8110961a60b4269
Buddy Agyin <agyin3@gmail.com>
2020-05-04 21:20:20 -0600

    add redux persistance

--- plza-fe/src/redux/store/index.js
+++ plza-fe/src/redux/store/index.js
@@ -2,4 +2,18 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+// Adding Redux Persistance
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+// Configure persistance to use local storage 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+// Return persisted redux reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export let store = createStore(persistedReducer, applyMiddleware(thunk))
+export let persistor = persistStore(store)
