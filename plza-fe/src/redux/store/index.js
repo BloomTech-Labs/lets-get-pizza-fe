911a658a30c4fc01e916d6f154eaedbc448c2e17
Buddy Agyin <agyin3@gmail.com>
2020-05-06 01:30:30 -0600

    create redux store filter

--- plza-fe/src/redux/store/index.js
+++ plza-fe/src/redux/store/index.js
@@ -6,6 +6,7 @@ import rootReducer from '../reducers'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import createEncryptor from 'redux-persist-transform-encrypt' // library used to encrypt redux store
+import { createFilter, createBlacklistFilter } from 'redux-persist-transform-filter'
 
 // Configure local storage encryptor 
 const encryptor = createEncryptor({
@@ -15,11 +16,20 @@ const encryptor = createEncryptor({
     }
 })
 
+// Blacklist `isLoading` and `error` keys from user reducer to prevent them from persisting
+const userBlackList = createBlacklistFilter(
+    'user',
+    ['isLoading', 'error']
+)
+
 // Configure persistance to use local storage 
 const persistConfig = {
     key: 'root',
     storage,
-    transforms: [encryptor]
+    transforms: [
+        userBlackList,
+        encryptor
+    ]
 }
 
 // Return persisted redux reducer
