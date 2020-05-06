399b8dfa9a9245c5606e1dbaf19fd4caf8a52e47
Buddy Agyin <agyin3@gmail.com>
2020-05-05 21:35:18 -0600

    encrypt redux store

--- plza-fe/src/redux/store/index.js
+++ plza-fe/src/redux/store/index.js
@@ -5,11 +5,21 @@ import rootReducer from '../reducers'
 // Adding Redux Persistance
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import createEncryptor from 'redux-persist-transform-encrypt' // library used to encrypt redux store
+
+const encryptor = createEncryptor({
+    secretKey: 'PizzaPlza24',
+    onError: function(error){
+        console.log(error)
+    }
+})
 
 // Configure persistance to use local storage 
 const persistConfig = {
     key: 'root',
     storage,
+    blacklist: ['user', 'locations'],
+    // transforms: [encryptor]
 }
 
 // Return persisted redux reducer
