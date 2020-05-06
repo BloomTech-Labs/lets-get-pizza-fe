c0b85ea866facc5a7fb9161e69e523dc5953799e
Buddy Agyin <agyin3@gmail.com>
2020-05-06 01:33:57 -0600

    replace history obj with window obj

--- plza-fe/src/redux/actions/userActions.js
+++ plza-fe/src/redux/actions/userActions.js
@@ -1,27 +1,27 @@
 import API from "../../utils/API"
 import * as types from '../types'
 
-export const userLogin = ({username, password}, history) => dispatch => {
+export const userLogin = ({username, password}) => dispatch => {
     dispatch({type: types.LOGIN_START, payload: true})
     API.post('/auth/user/login', {username, password})
         .then(res => {
             localStorage.setItem('token', JSON.stringify(res.data.token))
             dispatch({type: types.LOGIN_SUCCESS, payload: res.data.user})
-            history.push('/users/dash')
+            window.location.replace("/users/dash")
         })
         .catch(err => {
-            dispatch({type: types.LOGIN_FAIL, payload: false})
+            dispatch({type: types.LOGIN_FAIL, payload: 'Invalid username or password'})
         })
 }
 
-export const userRegister = (data, history) => dispatch => {
+export const userRegister = (data) => dispatch => {
     dispatch({type: types.REGISTER_START, payload: true})
     delete data.verify_password
     API.post('/auth/user/register', data)
         .then(res => {
             localStorage.setItem('token', JSON.stringify(res.data.token))
             dispatch({type: types.REGISTER_SUCCESS, payload: res.data.user})
-            history.push('/users/dash')
+            window.location.replace("/users/dash")
         })
         .catch(err => {
             dispatch({type: types.REGISTER_FAIL, payload: false})
