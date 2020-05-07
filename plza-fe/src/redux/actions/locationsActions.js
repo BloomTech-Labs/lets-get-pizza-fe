af8c1b2113ca83f1e5b7fee0eb0ebd5f0c7dca48
Buddy Agyin <agyin3@gmail.com>
2020-05-06 21:55:20 -0600

    create actions for login/registration

--- plza-fe/src/redux/actions/locationsActions.js
+++ plza-fe/src/redux/actions/locationsActions.js
@@ -11,6 +11,24 @@ export const locationLogin = ({username, password}) => dispatch => {
             window.location.replace('/')
         })
         .catch(err => {
-            dispatch({type: types.LOGIN_FAIL, payload: false})
+            dispatch({type: types.LOGIN_FAIL, payload: {error: 'Invalid username or password', isLoading: false}})
+        })
+}
+
+export const locationRegister = (data) => dispatch => {
+    dispatch({type: types.REGISTER_START, payload: true})
+    delete data.verify_password
+    API.post('auth/location/register', data)
+        .then(res => {
+            console.log(res)
+            localStorage.setItem('token', JSON.stringify(res.data.token))
+            dispatch({type: types.REGISTER_SUCCESS, payload: res.data.location})
+            window.location.replace('/')
+        })
+        .catch(err => {
+            dispatch({
+                type: types.REGISTER_FAIL, 
+                payload: {error: 'There was an error with your registration, please try again', isLoading: false}
+            })
         })
 }
--- plza-fe/src/redux/reducers/locationsReducer.js
+++ plza-fe/src/redux/reducers/locationsReducer.js
@@ -1,6 +1,7 @@
 import * as types from '../types'
 const initialState = {
-    isLoading: false
+    isLoading: false,
+    error: undefined
 }
 
 export const locationsReducer = (state = initialState, {type, payload}) => {
@@ -11,14 +12,18 @@ export const locationsReducer = (state = initialState, {type, payload}) => {
                 isLoading: payload
             }
         case types.LOGIN_SUCCESS:
+        case types.REGISTER_SUCCESS:
             return {
                 ...payload,
+                error: undefined,
                 isLoading: false
             }
         case types.LOGIN_FAIL:
+        case types.REGISTER_FAIL:
             return {
                 ...state,
-                isLoading: payload
+                error: payload.error,
+                isLoading: payload.isLoading
             }
         default:
             return state
