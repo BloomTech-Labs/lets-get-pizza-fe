b4da3a66f173a6a2195978b479698083180e16b2
Buddy Agyin <agyin3@gmail.com>
2020-05-07 22:17:18 -0600

    create action for claiming business

--- plza-fe/src/redux/reducers/locationsReducer.js
+++ plza-fe/src/redux/reducers/locationsReducer.js
@@ -1,25 +1,30 @@
-import * as types from '../types'
+import * as types from '../types/locationTypes'
 const initialState = {
     isLoading: false,
     error: undefined
 }
 
 export const locationsReducer = (state = initialState, {type, payload}) => {
+    console.log(type)
     switch (type) {
-        case types.LOGIN_START:
+        case types.LOCATION_LOGIN_START:
+        case types.LOCATION_REGISTER_START:
+        case types.LOCATION_CLAIM_START:
             return {
                 ...state,
                 isLoading: payload
             }
-        case types.LOGIN_SUCCESS:
-        case types.REGISTER_SUCCESS:
+        case types.LOCATION_LOGIN_SUCCESS:
+        case types.LOCATION_REGISTER_SUCCESS:
+        case types.LOCATION_CLAIM_SUCCESS:
             return {
                 ...payload,
                 error: undefined,
                 isLoading: false
             }
-        case types.LOGIN_FAIL:
-        case types.REGISTER_FAIL:
+        case types.LOCATION_LOGIN_FAIL:
+        case types.LOCATION_REGISTER_FAIL:
+        case types.LOCATION_CLAIM_FAIL:
             return {
                 ...state,
                 error: payload.error,
