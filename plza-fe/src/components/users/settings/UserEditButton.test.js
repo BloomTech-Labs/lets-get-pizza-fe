c892647c0f597c443451049dd12bb1bcf4cb7c8d
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-05-11 20:40:44 -0700

    Begin testing on users/settings components

---
+++ plza-fe/src/components/users/settings/UserEditButton.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+
+import { userToggleEdit } from '../../../redux/actions/userActions';
+import UserEditButton from './UserEditButton';
+
+
+const mockStore = configureStore([]);
+
+describe('UserEditButton', () => {
+    let store;
+    let component;
+    const item = {
+        name: 'itemName'
+    }
+    const text = 'textValue'
+    const event = { target: { id: 'itemName' } }
+
+    beforeEach(() => {
+        store = mockStore({
+            user: { field: 'currentItem' }
+        });
+
+        store.dispatch = jest.fn();
+
+        component = renderer.create(
+            <Provider store={store}>
+                <UserEditButton item={item} text={text} />
+            </Provider>
+        )
+    });
+
+    it('should render with text from props', () => {
+        expect(component.toJSON()).toMatchSnapshot();
+    })
+
+    it('should dispatch userToggleEdit action on button click', () => {
+        renderer.act(() => {
+            component.root.findByType('button').props.onClick();
+        });
+
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+        // expect(store.dispatch).toHaveBeenCalledWith(userToggleEdit(event, item.name));
+    });
+});
