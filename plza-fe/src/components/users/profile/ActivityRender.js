f8b27c6c000c9c4525017e7e154337e1a6113834
Buddy Agyin <agyin3@gmail.com>
2020-05-21 23:52:10 -0600

    add key to remove error

--- plza-fe/src/components/users/profile/ActivityRender.js
+++ plza-fe/src/components/users/profile/ActivityRender.js
@@ -5,11 +5,11 @@ import ReviewActivity from "./ReviewActivity";
 export default function ActivityRender(props) {
   return (
     <div style={{ width: "800px" }}>
-      {props.activity.map((evt) => {
+      {props.activity.map((evt, index) => {
         if (evt.start_time) {
-          return <EventsActivity user={props.user} evt={evt} />;
+          return <EventsActivity key={index} user={props.user} evt={evt} />;
         } else if (evt.rating) {
-          return <ReviewActivity user={props.user} evt={evt} />;
+          return <ReviewActivity key={index} user={props.user} evt={evt} />;
         }
       })}
     </div>
