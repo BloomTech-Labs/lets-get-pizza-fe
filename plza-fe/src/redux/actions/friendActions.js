4c4718c52341a4d44c0b7cb3b9d849496ba89f79
Buddy Agyin <agyin3@gmail.com>
2020-05-22 00:00:36 -0600

    create actions and types for friend events/reviews

--- plza-fe/src/redux/actions/friendActions.js
+++ plza-fe/src/redux/actions/friendActions.js
@@ -19,10 +19,40 @@ export const getFriendData = (username) => dispatch => {
                 dispatch({type: types.GET_FRIEND_SUCCESS, payload: res.data.users[0]})
                 return res.data.users[0].favorite_pizza_shop
             })
-            .then(favShop => {
-                dispatch(getFriendFavoriteShop(favShop))
-            })
             .catch(err => {
                 dispatch({type: types.GET_FRIEND_FAIL, payload: {isLoading: false}})
             })
+}
+
+// Events
+export const eventsByFriend = (id) => dispatch => {
+    dispatch({ type: types.FRIEND_EVENT_START, payload: true })
+    API.get(`/events/users/${id}`)
+        .then((res) => {
+            const currentDate = new Date().toISOString();
+            dispatch({
+                type: types.FRIEND_EVENT_SUCCESS,
+                payload: res.data
+                    .sort((a, b) => new Date(a.start_time) - new Date(b.start_time))
+                    .filter((date) => date.start_time > currentDate)
+            });
+        })
+        .catch((err) => {
+            dispatch({ type: types.FRIEND_EVENT_FAIL, payload: {isLoading: false} })
+        }); 
+}
+
+// Reviews
+export const reviewsByFriend = id => dispatch => {
+    dispatch({type: types.FRIEND_REVIEW_START, payload: true})
+    API.get(`/reviews/users/${id}`)
+        .then(res => {
+            dispatch({
+                type: types.FRIEND_REVIEW_SUCCESS,
+                payload: res.data.sort((a, b) => b.id - a.id)
+            })
+        })
+        .catch((err) => {
+            dispatch({ type: types.FRIEND_REVIEW_FAIL, payload: {isLoading: false} })
+        })
 }
--- plza-fe/src/redux/reducers/friendReducer.js
+++ plza-fe/src/redux/reducers/friendReducer.js
@@ -12,6 +12,8 @@ export const friendReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case types.GET_FRIEND_START:
         case types.GET_FRIEND_FAV_SHOP_START:
+        case types.FRIEND_REVIEW_START:
+        case types.FRIEND_EVENT_START:
             return {
                 ...state,
                 isLoading: payload
@@ -30,8 +32,22 @@ export const friendReducer = (state = initialState, { type, payload }) => {
                 },
                 isLoading: false
             }
+        case types.FRIEND_REVIEW_SUCCESS: 
+            return {
+                ...state,
+                reviews: payload,
+                isLoading: false
+            }
+        case types.FRIEND_EVENT_SUCCESS:
+            return {
+                ...state, 
+                events: payload,
+                isLoading: false
+            }
         case types.GET_FRIEND_FAIL:
         case types.GET_FRIEND_FAV_SHOP_FAIL:
+        case types.FRIEND_EVENT_FAIL:
+        case types.FRIEND_REVIEW_FAIL:
             return {
                 ...state,
                 isLoading: payload.isLoading
