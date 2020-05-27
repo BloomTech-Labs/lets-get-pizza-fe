6cea44bdca53860c7dcfa0587ef6147ac165cfb4
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-05-26 19:43:23 -0700

    Add basic UserEditActive tests

--- plza-fe/src/components/users/settings/UserEditActive.test.js
+++ plza-fe/src/components/users/settings/UserEditActive.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react'
 
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
+import { userEditSettings } from '../../../redux/actions/userActions';
 
 import UserEditActive from './UserEditActive';
 
 jest.mock('../../../redux/actions/userActions');
-const mockStore = configureStore({});
+const mockStore = configureStore();
+
 
 describe('UserEditActive with item.value as a string', () => {
+
     let store;
     let component;
     const item = {
@@ -17,6 +21,7 @@ describe('UserEditActive with item.value as a string', () => {
         name: "location",
         value: "Barcelona"
     };
+    const newValue = "Los Angeles"
     beforeEach(() => {
         store = mockStore({
             user: { field: 'location' }
@@ -36,18 +41,22 @@ describe('UserEditActive with item.value as a string', () => {
         expect(component.toJSON()).toMatchSnapshot();
     })
 
-    it('contains the proper title & value', () => {
-
+    it('input onChange can fire userEditSettings for each option & fires correct Redux action', () => {
+        renderer.act(() => {
+            component.root.findByType('input').props.onChange({ target: { name: item.name } }, { value: newValue })
+        });
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+        expect(store.dispatch).toHaveBeenCalledWith(userEditSettings({ target: { name: item.name } }, newValue));
     })
+
 })
 
 describe('UserEditActive with item.value as an array', () => {
+
     let store;
     let component;
     const item = {
-        icon: "ban",
-        name: "dietary_preference",
-        value: ["vegan", "gluten-free"]
+        icon: "ban"
     };
 
     beforeEach(() => {
@@ -66,5 +75,25 @@ describe('UserEditActive with item.value as an array', () => {
 
     it('should render with text from props', () => {
         expect(component.toJSON()).toMatchSnapshot();
+    })
+
+    it('Dropdown onChange can fire userEditSettings for each option & fires correct Redux action', () => {
+        const options = [
+            { text: "Vegan", value: "vegan" },
+            { text: "Gluten-Free", value: "gluten-free" },
+            { text: "Lacto-vegetarian", value: "lacto-vegetarian" },
+            { text: "Ovo-vegetarian", value: "ovo-vegetarian" },
+            { text: "Pescetarian", value: "pescetarian" },
+            { text: "Vegetarian", value: "vegetarian" },
+            { text: "None", value: "" },
+        ]
+        renderer.act(() => {
+            options.forEach(({ text, value }) => {
+                component.root.findAllByType('div')[2].props.onChange({ target: { text: text } }, { value })
+            })
+        });
+
+        expect(store.dispatch).toHaveBeenCalledTimes(options.length);
+        expect(store.dispatch).toHaveBeenCalledWith(userEditSettings({ target: { text: 'Vegan' } }, 'vegan'));
     })
 })
