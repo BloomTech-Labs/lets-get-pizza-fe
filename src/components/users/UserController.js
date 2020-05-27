7e31288c870bf8dc7d6b77bf5cda435b70fb64c8
Buddy Agyin <agyin3@gmail.com>
2020-05-27 00:13:42 -0600

    add route for user search component

--- src/components/users/UserController.js
+++ src/components/users/UserController.js
@@ -5,6 +5,7 @@ import Login from "./Login";
 import Register from "./Register";
 import Dashboard from './Dashboard';
 import PrivateRoute from "../../components/PrivateRoute";
+import UserSearch from "./search/UserSearch";
 
 
 const UserController = () => (
@@ -17,8 +18,11 @@ const UserController = () => (
       <Register />
     </Route>
   
-  
-    <PrivateRoute path = "/users/dash" component = {Dashboard} /> 
+    <PrivateRoute path = "/users/dash" component = {Dashboard} />
+    
+    <Route path="/users/search">
+      <UserSearch />
+    </Route>
   </Switch>
 );
 
