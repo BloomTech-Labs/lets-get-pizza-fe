d834a347fc5644e965c7e8e0d9c7a5e9ab5dd5b1
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-06-09 20:25:28 -0700

    setting up tests and initial testing for profile page

--- src/components/users/profile/ActivityContainer.js
+++ src/components/users/profile/ActivityContainer.js
@@ -1,45 +1,52 @@
-import React, { useEffect } from 'react'
-import { Grid, Header } from 'semantic-ui-react'
-import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import { eventsByFriend, reviewsByFriend, getFriendFavoriteShop } from '../../../redux/actions/friendActions';
-import { eventsByUser, locationByUser, reviewsByUser } from '../../../redux/actions/userActions';
-import ActivityFeed from './ActivityFeed';
-import FriendsList from '../FriendsComp/FriendsList';
+import React, { useEffect } from "react";
+import { Grid, Header } from "semantic-ui-react";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import {
+  eventsByFriend,
+  reviewsByFriend,
+  getFriendFavoriteShop,
+} from "../../../redux/actions/friendActions";
+import {
+  eventsByUser,
+  locationByUser,
+  reviewsByUser,
+} from "../../../redux/actions/userActions";
+import ActivityFeed from "./ActivityFeed";
+import FriendsList from "../FriendsComp/FriendsList";
 
 const ActivityContainer = ({ user }) => {
-    const { pathname } = useLocation()
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      // dispatch user actions if path includes 'dash'
-      if(pathname.includes('dash')){
-        dispatch(eventsByUser(user.id));
-        dispatch(locationByUser(user.favorite_pizza_shop));
-        dispatch(reviewsByUser(user.id));
-      }else{
-        // dispatch friend actions if not on dashboard
-        dispatch(eventsByFriend(user.id));
-        dispatch(getFriendFavoriteShop(user.favorite_pizza_shop));
-        dispatch(reviewsByFriend(user.id));
-      }
-    }, [user.id, user.favorite_pizza_shop]);
-    const activities = user.reviews.concat(user.events);
-    return (
-        <Grid.Row>
-            {!pathname.includes('friends') ? 
-            (
-            <>
-              <Header size='huge' textAlign='center' style={{width: '100%'}}>Recent Activity</Header>
-              <ActivityFeed activities={activities} user={user} />
-            </>
-            ):
-            (
-            <FriendsList user={user} />
-            )
-            }
-        </Grid.Row>
-    )
-}
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
 
-export default ActivityContainer
+  useEffect(() => {
+    // dispatch user actions if path includes 'dash'
+    if (pathname.includes("dash")) {
+      dispatch(eventsByUser(user.id));
+      dispatch(locationByUser(user.favorite_pizza_shop));
+      dispatch(reviewsByUser(user.id));
+    } else {
+      // dispatch friend actions if not on dashboard
+      dispatch(eventsByFriend(user.id));
+      dispatch(getFriendFavoriteShop(user.favorite_pizza_shop));
+      dispatch(reviewsByFriend(user.id));
+    }
+  }, [user.id, user.favorite_pizza_shop]);
+  const activities = user.reviews.concat(user.events);
+  return (
+    <Grid.Row>
+      {!pathname.includes("friends") ? (
+        <>
+          <Header size="huge" textAlign="center" style={{ width: "100%" }}>
+            Recent Activity
+          </Header>
+          <ActivityFeed activities={activities} user={user} />
+        </>
+      ) : (
+        <FriendsList user={user} />
+      )}
+    </Grid.Row>
+  );
+};
+
+export default ActivityContainer;
--- src/components/users/profile/ProfileMain.js
+++ src/components/users/profile/ProfileMain.js
@@ -1,26 +1,25 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import ProfileDetails from './ProfileDetails';
-import API from '../../../utils/API';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import ProfileDetails from "./ProfileDetails";
+import API from "../../../utils/API";
 
 const ProfileMain = () => {
-    const [user, username] = useSelector(({ user }) => [user, user.username]);
-    
-    useEffect(() => {
-        API.post(`/auth/user/refresh`, { username })
-            .then(res => {
-                localStorage.setItem('token', JSON.stringify(res.data.token))
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [])
-    return(
-        <div>
-            <ProfileDetails user={user} />
-        </div>
-    )
-}
+  const [user, username] = useSelector(({ user }) => [user, user.username]);
 
+  useEffect(() => {
+    API.post(`/auth/user/refresh`, { username })
+      .then((res) => {
+        localStorage.setItem("token", JSON.stringify(res.data.token));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div>
+      <ProfileDetails user={user} />
+    </div>
+  );
+};
 
-export default ProfileMain
+export default ProfileMain;
