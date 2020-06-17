8c6621f72a71a01eccfb8d96307e0b72ceb78a63
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-06-16 21:44:36 -0700

    Login/Registration userActions test complete

--- src/redux/actions/userActions.test.js
+++ src/redux/actions/userActions.test.js
@@ -1,37 +1,64 @@
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import * as actions from './userActions'
-import * as types from '../types/userTypes'
-import fetchMock from 'fetch-mock'
+import * as actions from "./userActions";
+import * as types from "../types/userTypes";
+import fetchMock from "fetch-mock";
+import API from "../../utils/API";
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+describe("authroization functions", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
 
+  it("creates USER_LOGIN_START and USER_LOGIN_SUCCESS when API POST is complete", async () => {
+    const creds = {
+      username: "JDawg",
+      password: "1234",
+    };
 
-describe("test userActions init", () => {
-    afterEach(() => {
-        fetchMock.restore()
-      })
-    
-    it("init passes", () => {
-        expect(1).toBe(1);
-    })
+    const expectedActions = [
+      { type: types.USER_LOGIN_START, payload: true },
+      { type: types.USER_LOGIN_SUCCESS },
+    ];
 
-    it('creates FETCH_TODOS_SUCCESS when fetching todos has been done', () => {
-        fetchMock.getOnce('/todos', {
-          body: { todos: ['do something'] },
-          headers: { 'content-type': 'application/json' }
-        })
-    
-        const expectedActions = [
-          { type: types.USER_LOGIN_START },
-          { type: types.USER_LOGIN_SUCCESS, body: { todos: ['do something'] } }
-        ]
-        const store = mockStore({ todos: [] })
-    
-        // return store.dispatch(actions.fetchTodos()).then(() => {
-        //   // return of async actions
-        //   expect(store.getActions()).toEqual(expectedActions)
-        // })
-      })
-})
+    API.post = jest.fn((url) => {
+      return Promise.resolve();
+    });
+
+    const dispatch = jest.fn();
+    const getState = jest.fn(() => {
+      url: "/auth/user/login";
+    });
+
+    await actions.userLogin(creds)(dispatch, getState);
+
+    dispatch.mock.calls.forEach((call, idx) => {
+      expect(call[0]).toEqual(expectedActions[idx]);
+    });
+  });
+
+  it("creates USER_REGISTER_START and USER_REGISTER_SUCCESS when API POST is complete", async () => {
+    const creds = {
+      username: "newguy",
+      password: "4321",
+    };
+
+    const expectedActions = [
+      { type: types.USER_REGISTER_START, payload: true },
+      { type: types.USER_REGISTER_SUCCESS },
+    ];
+
+    API.post = jest.fn((url) => {
+      return Promise.resolve();
+    });
+
+    const dispatch = jest.fn();
+    const getState = jest.fn(() => {
+      url: "/auth/user/register";
+    });
+
+    await actions.userRegister(creds)(dispatch, getState);
+
+    dispatch.mock.calls.forEach((call, idx) => {
+      expect(call[0]).toEqual(expectedActions[idx]);
+    });
+  });
+});
