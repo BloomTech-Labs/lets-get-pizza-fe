eca2e0280ce34a917929bfdb361b0a345d506b2b
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-06-16 22:15:38 -0700

    Complete tests for userToggleEdit & userEditSettings

--- src/redux/actions/userActions.test.js
+++ src/redux/actions/userActions.test.js
@@ -3,12 +3,12 @@ import * as types from "../types/userTypes";
 import fetchMock from "fetch-mock";
 import API from "../../utils/API";
 
-describe("authroization functions", () => {
+describe("Users authroization actions", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it("creates USER_LOGIN_START and USER_LOGIN_SUCCESS when API POST is complete", async () => {
+  it("creates USER_LOGIN_START and USER_LOGIN_SUCCESS when API login POST is complete", async () => {
     const creds = {
       username: "JDawg",
       password: "1234",
@@ -35,7 +35,7 @@ describe("authroization functions", () => {
     });
   });
 
-  it("creates USER_REGISTER_START and USER_REGISTER_SUCCESS when API POST is complete", async () => {
+  it("creates USER_REGISTER_START and USER_REGISTER_SUCCESS when API register POST is complete", async () => {
     const creds = {
       username: "newguy",
       password: "4321",
@@ -60,5 +60,78 @@ describe("authroization functions", () => {
     dispatch.mock.calls.forEach((call, idx) => {
       expect(call[0]).toEqual(expectedActions[idx]);
     });
+  });
+});
+
+//
+describe("Users settings actions", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it("creates TOGGLE_EDIT that passes in an empty string if field & id are the same", () => {
+    const input = {
+      event: { target: { id: "location" } },
+      field: "location",
+    };
+
+    const dispatch = jest.fn();
+    const getState = jest.fn();
+
+    const expectedAction = { type: types.TOGGLE_EDIT, payload: "" };
+
+    actions.userToggleEdit(input.event, input.field)(dispatch, getState);
+    expect(dispatch.mock.calls[0][0]).toEqual(expectedAction);
+  });
+
+  it("creates TOGGLE_EDIT that passes in the given id if field & id are NOT the same", () => {
+    const input = {
+      event: { target: { id: "location" } },
+      field: "dietary_preference",
+    };
+
+    const dispatch = jest.fn();
+    const getState = jest.fn();
+
+    const expectedAction = { type: types.TOGGLE_EDIT, payload: input.event.target.id };
+
+    actions.userToggleEdit(input.event, input.field)(dispatch, getState);
+    expect(dispatch.mock.calls[0][0]).toEqual(expectedAction);
+  });
+
+  it("creates EDIT_SETTINGS with payload for editing dietary_preference", () => {
+    const input1 = {
+      event: { target: {} },
+      value: "vegan",
+    };
+
+    const dispatch = jest.fn();
+    const getState = jest.fn();
+
+    const expectedAction = {
+      type: types.EDIT_SETTINGS,
+      payload: { dietary_preference: "vegan" },
+    };
+
+    actions.userEditSettings(input1.event, input1.value)(dispatch, getState);
+    expect(dispatch.mock.calls[0][0]).toEqual(expectedAction);
+  });
+
+  it("creates EDIT_SETTINGS with payload for all other user dashboard preferences", () => {
+    const input = {
+      event: { target: { name: "location" } },
+      value: "L.A.",
+    };
+
+    const dispatch = jest.fn();
+    const getState = jest.fn();
+
+    const expectedAction = {
+      type: types.EDIT_SETTINGS,
+      payload: { location: "L.A." },
+    };
+
+    actions.userEditSettings(input.event, input.value)(dispatch, getState);
+    expect(dispatch.mock.calls[0][0]).toEqual(expectedAction);
   });
 });
