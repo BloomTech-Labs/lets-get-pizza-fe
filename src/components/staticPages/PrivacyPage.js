0650eed5d5d5b6ae8c61884985c5084b664269cd
Your Name <you@example.com>
2020-06-17 00:25:49 -0400

    privacy-page started

---
+++ src/components/staticPages/PrivacyPage.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function PrivacyPage () {
+    return (
+        <>
+            <div>
+                <h3>PLZA PRIVACY POLICY</h3> 
+                <h1>When you use our services, you’re trusting us with your
+                    information. We understand this is a big responsibility and
+                    work hard to protect your information and put you in
+                    control.
+                </h1>
+                <p>This Privacy Policy is meant to help you understand what information we collect, why we collect it,
+                    and how you can update, manage, export, and delete your information.
+                    <br/>
+                    Effective March 31, 2020
+                </p>
+            </div>
+
+            <div>
+                <h2>We want you to understand the types of information we
+                    collect as you use our services</h2>
+                <p>We collect information to provide better services to all our users — from figuring out basic stuff like
+                    your food preferences, to more complex things like which ads you’ll find most useful, the people
+                    who matter most to you online, or which Pizza places you might like.
+                    <br />
+                    When you’re not signed in to your Plza Account, we store the information we collect with unique
+                    identifiers tied to the browser, application, or device you’re using. This helps us do things like
+                    maintain your language preferences across browsing sessions.
+                    When you’re signed in, we also collect information that we store with your Plza Account, which we
+                    treat as personal information.
+                </p>
+            </div>
+
+            <div>
+                <h2>Things you create or provide to us</h2>
+                <p>jklh;fa;jkhf</p>
+            </div>
+
+            <div>
+                <h2>jf'adkljad'skj</h2>
+                <p>jklh;fa;jkhf</p>
+            </div>
+
+            <div>
+                <h2>jf'adkljad'skj</h2>
+                <p>jklh;fa;jkhf</p>
+            </div>
+        </>
+    )
+ }
+    
